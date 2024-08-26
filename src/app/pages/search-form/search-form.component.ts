aba22fe8a1cd0b91c7bf2b2f95b46c7f4d98c630
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2024-08-26 12:18:17 +0200

    favorites working

--- src/app/pages/search-form/search-form.component.ts
+++ src/app/pages/search-form/search-form.component.ts
@@ -1,26 +1,39 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { GetCharacterService } from '../../services/get-character.service';
+import { Character } from '../../models/characters-array.model';
+import { CharacterCardComponent } from '../character-card/character-card.component';
+import { FavoriteCharactersService } from '../../services/favorite-characters.service';
 
 @Component({
   selector: 'app-search-form',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, CharacterCardComponent],
   templateUrl: './search-form.component.html',
   styleUrl: './search-form.component.css'
 })
 export class SearchFormComponent {
 
   characterForm: FormGroup;
+  characterList!: Character[];
+  private _characterApiService = inject(GetCharacterService);
 
-  constructor(private form: FormBuilder){
-    this.characterForm = this.form.group({
-      characterName:['', Validators.required]
-    })
+  constructor(
+    private formBuilder: FormBuilder,
+    private favoriteService: FavoriteCharactersService
+  ) {
+    this.characterForm = this.formBuilder.group({characterName: ['', Validators.required]});
+  }
+
+  onFavoriteSelected(character: Character) {
+    this.favoriteService.addFavorite(character);
   }
 
   sendData(){
-    console.log(this.characterForm.value)
+    this._characterApiService.getCharacterByName(this.characterForm.value.characterName).subscribe((data:Character[])=>{
+      this.characterList = data;
+    })
   }
 
 }
