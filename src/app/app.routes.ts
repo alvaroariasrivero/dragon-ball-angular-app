7c1554f688b0e80377994281f4dac723f067a41b
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2024-08-29 14:05:27 +0200

    routes updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { FavoritesComponent } from './pages/favorites/favorites.component';
 import { SearchFormComponent } from './pages/search-form/search-form.component';
+import { CharacterComponent } from './pages/character/character.component';
 
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'favorites', component: FavoritesComponent},
-    {path: 'search', component: SearchFormComponent}
+    {path: 'search', component: SearchFormComponent},
+    {path: 'character/:characterID', component: CharacterComponent}
 ];
